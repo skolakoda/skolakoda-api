d02eb97150c8838ee37a9bf369ab4631c196ba49
Damjan Pavlica <mudroljub@gmail.com>
2017-03-22 20:14:08 +0100

    demo

--- web/index.php
+++ web/index.php
@@ -15,11 +15,16 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
 
-// Our web handlers
+/* RUTE */
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
+});
+
+$app->get('/cowsay', function() use($app) {
+  $app['monolog']->addDebug('cowsay');
+  return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
 });
 
 $app->run();
