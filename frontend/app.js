16510fbc8c0a2f73a2031886d4982ec8b253c553
Damjan Pavlica <mudroljub@gmail.com>
2017-05-17 02:15:34 +0200

    frontend postavljen

--- frontend/app.js
+++ frontend/app.js
@@ -9,6 +9,30 @@ class Korisnici {
   }
 }
 
+class Kursevi {
+  constructor($http) {
+    $http
+      .get('https://skolakoda-api.herokuapp.com/kursevi')
+      .then(
+        response => this.kursevi = response.data,
+        error => console.log(error)
+      )
+  }
+}
+
+class Prijave {
+  constructor($http) {
+    $http
+      .get('https://skolakoda-api.herokuapp.com/prijave')
+      .then(
+        response => this.prijave = response.data,
+        error => console.log(error)
+      )
+  }
+}
+
 angular
   .module('adminApp', [])
   .controller('Korisnici', $http => new Korisnici($http))
+  .controller('Kursevi', $http => new Kursevi($http))
+  .controller('Prijave', $http => new Prijave($http))  
--- web/index.php
+++ web/index.php
@@ -4,12 +4,8 @@ require('../vendor/autoload.php');
 
 $app = new Silex\Application();
 $app['debug'] = true;
+
 $dbopts = parse_url(getenv('DATABASE_URL'));
-
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
 $app->register(
   new Herrera\Pdo\PdoServiceProvider(),
    array(
