8021c7c048926cb2ca19755420afd72ac9fe7c21
Damjan Pavlica <mudroljub@gmail.com>
2017-05-18 10:29:47 +0200

    podaci asocirani, sve radi

--- frontend/app.js
+++ frontend/app.js
@@ -1,17 +1,32 @@
 class Admin {
-  constructor($http) {
-    $http
-      .get('https://skolakoda-api.herokuapp.com/korisnici')
-      .then(response => this.korisnici = response.data)
-    $http
-      .get('https://skolakoda-api.herokuapp.com/kursevi')
-      .then(response => this.kursevi = response.data)
-    $http
-      .get('https://skolakoda-api.herokuapp.com/prijave')
-      .then(response => this.prijave = response.data)
+  constructor($q, $http) {
+    $q.all([
+      $http.get('https://skolakoda-api.herokuapp.com/korisnici'),
+      $http.get('https://skolakoda-api.herokuapp.com/kursevi'),
+      $http.get('https://skolakoda-api.herokuapp.com/prijave')
+    ])
+      .then(responses => {
+        this.korisnici = responses[0].data
+        this.kursevi = responses[1].data
+        this.prijave = responses[2].data
+        this.asocirajPodatke()
+      })
+  }
+
+  asocirajPodatke() {
+    this.prijave.map(prijava => {
+      this.kursevi.map(kurs => {
+        if (prijava.kurs_id == kurs.id)
+          return prijava.kurs_naziv = kurs.naziv
+      })
+      this.korisnici.map(korisnik => {
+        if (prijava.korisnik_id == korisnik.id)
+          return prijava.korisnik_ime = korisnik.ime
+      })
+    })
   }
 }
 
 angular
   .module('adminApp', [])
-  .controller('Admin', $http => new Admin($http))
+  .controller('Admin', ($q, $http) => new Admin($q, $http))
