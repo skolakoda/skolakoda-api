875bf422b2cff070ecbf59fe452d50ed5a960038
mudroljub <mudroljub@gmail.com>
2018-10-04 20:17:14 +0200

    lozinka

--- web/index.php
+++ web/index.php
@@ -18,6 +18,7 @@ $app->register(
 /* GET */
 
 $app->get('/korisnici', function() use($app) {
+  if ($_GET["lozinka"] != getenv('LOZINKA')) return "Lozinka nije ispravna";
   $upit = $app['pdo']->prepare('SELECT * FROM korisnici');
   $upit->execute();
   $korisnici = array();
@@ -28,6 +29,7 @@ $app->get('/korisnici', function() use($app) {
 });
 
 $app->get('/kursevi', function() use($app) {
+  if ($_GET["lozinka"] != getenv('LOZINKA')) return "Lozinka nije ispravna";
   $upit = $app['pdo']->prepare('SELECT * FROM kursevi');
   $upit->execute();
   $kursevi = array();
@@ -38,6 +40,7 @@ $app->get('/kursevi', function() use($app) {
 });
 
 $app->get('/prijave', function() use($app) {
+  if ($_GET["lozinka"] != getenv('LOZINKA')) return "Lozinka nije ispravna";
   $upit = $app['pdo']->prepare('SELECT * FROM prijave');
   $upit->execute();
   $prijave = array();
